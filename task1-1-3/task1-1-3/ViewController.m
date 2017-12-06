030c9f97e4999ff5e43ce253b80530188e589de3
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-06 12:32:49 +0900

    1.1.3条件分岐、ループの課題が完了しました。

---
+++ task1-1-3/task1-1-3/ViewController.m
@@ -0,0 +1,78 @@
+//
+//  ViewController.m
+//  task1-1-3
+//
+//  Created by 新井岩生 on 2017/12/06.
+//  Copyright © 2017年 新井岩生. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // if文
+    BOOL answer = YES;
+    if(answer==YES){
+        NSLog(@"yes");
+    }
+    
+    // if~else文
+    NSInteger num = 5;
+    if(num < 0){
+        NSLog(@"message1");
+    }else{
+        NSLog(@"message2"); //こちらのみ実行される
+    }
+    
+    // 三項演算子
+    BOOL request = YES;
+    NSLog(request ? @"Yes" : @"No");
+    
+    // for文
+    NSDictionary *pc = @{@"mac":@"マック", @"windows":@"ウインドウズ"};
+    for (id key in [pc keyEnumerator]) {
+        NSLog(@"%@", [pc valueForKey:key]);
+    }
+    
+    
+    // 高速列挙構文
+    NSArray *fruits = @[@"りんご", @"なし",@"みかん"];
+    for(id s in fruits)
+    {
+        NSLog(@"%@",(NSString*)s);
+    }
+
+    NSDictionary *smartphone = @{@"iphone":@"アイフォーン", @"android":@"アンドロイド"};
+    for (id key in [smartphone keyEnumerator]) {
+        NSLog(@"%@", [smartphone valueForKey:key]);
+    }
+    
+    // switch文
+    NSInteger number = 2;
+    switch (number) {
+        case 1:
+            NSLog(@"1");
+            break;
+        case 2:
+            NSLog(@"2");
+            break;
+        default:
+            NSLog(@"default");
+            break;
+    }
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
