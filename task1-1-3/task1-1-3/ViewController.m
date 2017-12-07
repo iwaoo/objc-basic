05d3145af9eefcc58a9eeb673f6427653e61048e
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-07 10:04:48 +0900

    odified

--- task1-1-3/task1-1-3/ViewController.m
+++ task1-1-3/task1-1-3/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  task1-1-3
-//
-//  Created by 新井岩生 on 2017/12/06.
-//  Copyright © 2017年 新井岩生. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -47,7 +39,7 @@
     {
         NSLog(@"%@",(NSString*)s);
     }
-
+    
     NSDictionary *smartphone = @{@"iphone":@"アイフォーン", @"android":@"アンドロイド"};
     for (id key in [smartphone keyEnumerator]) {
         NSLog(@"%@", [smartphone valueForKey:key]);
