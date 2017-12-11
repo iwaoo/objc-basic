51c458a718f9c2213153e98215311166f691df60
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-11 18:28:45 +0900

    216UIWebViewの課題の完了しました。

---
+++ task2-1-6/task2-1-6/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  task2-1-6
+//
+//  Created by 新井岩生 on 2017/12/11.
+//  Copyright © 2017年 新井岩生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UIWebViewDelegate>
+
+    @property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
