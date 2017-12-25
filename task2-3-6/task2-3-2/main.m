d877516d7c24679f1f5535a3dae72caec1038464
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-25 22:11:11 +0900

    2.3.6の課題が完了しました。

---
+++ task2-3-6/task2-3-2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  task2-3-2
+//
+//  Created by 新井岩生 on 2017/12/20.
+//  Copyright © 2017年 新井岩生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
