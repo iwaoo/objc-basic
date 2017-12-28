18db56604f247c51db4ce5ff1bb66aa795330468
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-28 13:12:29 +0900

    2.1.6の修正

---
+++ task2-1-6/task2-1-6Tests/task2_1_6Tests.m
@@ -0,0 +1,39 @@
+//
+//  task2_1_6Tests.m
+//  task2-1-6Tests
+//
+//  Created by 新井岩生 on 2017/12/11.
+//  Copyright © 2017年 新井岩生. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface task2_1_6Tests : XCTestCase
+
+@end
+
+@implementation task2_1_6Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
