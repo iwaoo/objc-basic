c957b73fd1ce205a7e8bf99d5a923f3d69b2a22c
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-28 10:47:39 +0900

    1.1.5の修正

---
+++ task1-1-5/task1-1-5/Account.m
@@ -0,0 +1,38 @@
+//
+//  Account.m
+//  task114
+//
+//  Created by 新井岩生 on 2017/12/07.
+//  Copyright © 2017年 新井岩生. All rights reserved.
+//
+
+#import "Account.h"
+#import "FavoriteProgrammingLanguage.h"
+
+@interface Account () <FavoriteProgrammingLanguageDelegate>
+
+@end
+
+@implementation Account
+
+// インターンに参加メドッド
+- (void) callData{
+    FavoriteProgrammingLanguage *favoriteProgrammingLanguage = [FavoriteProgrammingLanguage new];
+    favoriteProgrammingLanguage.delegate = self;
+    [favoriteProgrammingLanguage throwDelegate:@"インターンに参加する"];
+    NSLog(@"%@", [favoriteProgrammingLanguage throwDelegate:@"インターンに参加する"]);
+}
+
+// bj-cができる通知
+- (void)catchDelegate:(NSString *)string
+{
+    NSLog(@"%@", string);
+}
+
+@end
+
+
+
+
+
+
