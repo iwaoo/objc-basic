bdcb9f987ce71e3c0007d5ac68781c1909336e29
Iwao Arai <araiiwao.fp@gmail.com>
2017-12-28 16:37:43 +0900

    2.1.14の修正

--- task2-1-14/Test/ViewController.m
+++ task2-1-14/Test/ViewController.m
@@ -9,9 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+    @property (weak, nonatomic) IBOutlet UITableView *table;
+    @property(nonatomic) NSArray *item1;
+    @property(nonatomic) NSArray *item2;
+    @property(nonatomic) NSArray *item3;
+    @property(nonatomic) NSArray *item4;
 
-    @property (nonatomic, retain) NSMutableArray *imageNameArray;
-@property (weak, nonatomic) IBOutlet UITableView *table;
+    @property(nonatomic) NSInteger numberOfRows;
 
 @end
 
@@ -19,32 +23,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self arraySetUp];
-}
-
-
-- (void)arraySetUp {
-    self.imageNameArray = [NSMutableArray arrayWithArray: @[@"image1",@"image2",@"image3",@"image4"]];
-}
-
-- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.imageNameArray.count;
-}
-
-- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     // plistデータを読み込む
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"Property List" ofType:@"plist"];
     NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSArray *item1 = [dic objectForKey:@"item1"];
+    NSArray *item2 = [dic objectForKey:@"item2"];
+    NSArray *item3 = [dic objectForKey:@"item3"];
+    NSArray *item4 = [dic objectForKey:@"item4"];
+    self.item1 = item1;
+    self.item2 = item2;
+    self.item3 = item3;
+    self.item4 = item4;
+    
+    self.numberOfRows = [dic count];
+    self.navigationItem.title = @"ナビゲーションバー";
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.numberOfRows;
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+
     // データをテーブルセルに書き込む
     static NSString *cellId = @"cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
-    if ((int)indexPath.row % 2 != 1) {
-        cell.textLabel.numberOfLines = 0;// 文字の時はセルの高さ可変
-        cell.textLabel.text = [dic objectForKey:_imageNameArray[indexPath.row]];
-    } else {
-        cell.imageView.image = [UIImage imageNamed:[dic objectForKey:_imageNameArray[indexPath.row]]];
+
+    cell.textLabel.numberOfLines = 0;// 文字の時はセルの高さ可変
+
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = self.item1[1];
+            cell.imageView.image = [UIImage imageNamed:self.item1[0]];
+            break;
+        case 1:
+            cell.textLabel.text = self.item2[1];
+            cell.imageView.image = [UIImage imageNamed:self.item2[0]];
+            break;
+        case 2:
+            cell.textLabel.text = self.item3[1];
+            cell.imageView.image = [UIImage imageNamed:self.item3[0]];
+            break;
+        case 3:
+            cell.textLabel.text = self.item4[1];
+            cell.imageView.image = [UIImage imageNamed:self.item4[0]];
+            break;
+        default:
+            break;
     }
+
+
     cell.detailTextLabel.text = [NSString stringWithFormat:@""];
     
     return cell;
