e92fbf052ccbb40703539ab0b6ccb8bf5e271f9e
Iwao Arai <araiiwao.fp@gmail.com>
2018-01-11 19:38:06 +0900

    2.5.7  地図を取得し、緯度・経度を取得する 。の課題が完了しました。

---
+++ task2-5-7/task2-5-7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  task2-5-7
+//
+//  Created by 新井岩生 on 2018/01/11.
+//  Copyright © 2018年 新井岩生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
