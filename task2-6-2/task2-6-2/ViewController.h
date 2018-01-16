89561e79acbece5ac2d5a159df57590bfbc6d017
Iwao Arai <araiiwao.fp@gmail.com>
2018-01-16 14:09:39 +0900

    No50 2.6.2  スキーマを定義し、自作アプリをパラメータ付きで起動する。の課題が完了しました。

---
+++ task2-6-2/task2-6-2/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  task2-6-2
+//
+//  Created by 新井岩生 on 2018/01/16.
+//  Copyright © 2018年 新井岩生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *commentLabel;
+
+
+@end
+
